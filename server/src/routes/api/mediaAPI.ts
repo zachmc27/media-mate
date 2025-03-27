4e897493c1dc5eaf017fb30664c974217760f374
Bread <brett.mcbryde@yahoo.com>
2025-03-26 22:26:19 -0500

    added client API ts files

--- server/src/routes/api/mediaAPI.ts
+++ server/src/routes/api/mediaAPI.ts
@@ -2,20 +2,52 @@ import dotenv from 'dotenv';
 import { Media, TMDBKeywordResponse } from '../../models/media.js'; 
 dotenv.config();
 
-const BEARER_KEY = process.env.BearerTkn;
 
 // hi
-// interface Media {
-//     id: number;
-//     title: string;
-//     overview: string;
-//     description: string;
-//     poster_path: string;
-//     backdrop_path: string;
-//     release_date: string;
-//     vote_average: number;
-//     trailerKey: string;
-// }
+interface Media {
+    id: number;
+    title: string;
+    overview: string;
+    description: string;
+    poster_path: string;
+    backdrop_path: string;
+    release_date: string;
+    vote_average: number;
+    trailerKey: string;
+}
+
+// This function is used to get the popular movies via API based on a keyword search.
+// expected return :an array of Media objects that contain the keyword
+export async function keyWordSearch(keyword: string): Promise<Media[]> {
+    // grabs the TMDB Token from the .env file
+    const BearerTkn = process.env.TMDB_Token;
+    // const api_key = process.env.TMDB_API_KEY;
+    // grabs the base URL from the .env file
+    const baseUrl = process.env.TMDB_BASE_URL;
+    // creates the URL for the search
+    const url = `${baseUrl}/search/multi?api_key=8b9dce6c0b0d9a05e530a8cef23bb51&query=${keyword}`;
+    // `${baseUrl}/search/multi?api_key=${api_key}&query=${keyword}&include_adult=false&language=en-US&page=1`;
+    
+  
+    const options = {
+        method: 'GET',
+        // sets the headers for the request
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${BearerTkn}`
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const searchResults = await response.json();
+        console.log(searchResults);
+        return searchResults; // Adjust this return statement based on the expected Media[] structure
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
 
 // This function is used to get the details for a specific movie via API and also appends the getTrailerKey function to the mediaDetails object
 export async function getMediaDetails(id: number, type: string): Promise<Media[]> {
@@ -26,6 +58,7 @@ export async function getMediaDetails(id: number, type: string): Promise<Media[]
         method: 'GET',
         headers: {
             accept: 'application/json',
+            Authorization: `Bearer ${BEARER_KEY}`
             Authorization: `Bearer ${BEARER_KEY}`
         }
     };
@@ -98,6 +131,7 @@ async function getTrailerKey(id: number, type: string): Promise<string> {
         method: 'GET',
         headers: {
             accept: 'application/json',
+            Authorization: `Bearer ${BEARER_KEY}`
             Authorization: `Bearer ${BEARER_KEY}`
         }
     };
