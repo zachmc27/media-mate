1a85840d9050739d17064a76d8a26cca8aef15d8
bmcbryde3 <brett.mcbryde@yahoo.com>
2025-04-01 18:34:26 -0500

    Update server.ts to fix the React url issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,23 @@
 const forceDatabaseRefresh = false;
-import path from "node:path";
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+import sequelize from './config/connection.js';
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static(path.resolve(__dirname, "../../client/dist")));
 
 app.use(express.json());
 app.use(routes);
