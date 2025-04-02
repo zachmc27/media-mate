e43bd55c9a73a8044ae59202b68030f687520c73
Sammi Teel <sammijteel@gmail.com>
2025-04-01 21:10:58 -0500

    Css globals and changes

---
+++ client/src/pages/StyleTestPage.tsx
@@ -0,0 +1,16 @@
+import "../App.css";
+
+function Test(){
+
+
+    return(
+        <div className="padding-root">
+            <p className="title-md-light">Wowza</p>
+            <button className="btn-lined padding-root">Log In</button>
+            <button className="btn-fill padding-root">Sign In</button>
+            <p className="p-light">Ready Loser? <a href="#">Get In.</a></p>
+    </div>
+    )
+}
+
+export default Test
