ef5343247cd4d99ca327fa943bc3358859b2fe0e
Sammi Teel <sammijteel@gmail.com>
2025-04-03 09:39:49 -0500

    Discover tease fix

--- client/src/components/Flicktease.tsx
+++ client/src/components/Flicktease.tsx
@@ -5,6 +5,15 @@ import { useEffect, useState } from "react";
 import { getFlickPicksList } from "../api/flickPicksAPI";
 import { Flickpick } from "../interfaces/FlickpickInterface";
 
+import drama from "../assets/DramaListIcon.png";
+import horror from "../assets/HORROR.png";
+import comedy from "../assets/COMEDY.png";
+import action from "../assets/ACTION.png";
+import romance from "../assets/ROMANCE.png";
+import thriller from "../assets/THRILLER.png";
+import fantasy from "../assets/FANTASY.png";
+import scifi from "../assets/SCIFI.png";
+
 export default function Flicktease() {
     const [flickpickList, setFlickpickList] = useState<Flickpick[] | null>(null);
     const [error, setError] = useState<string | null>(null);
@@ -41,7 +50,15 @@ export default function Flicktease() {
 
           <div key={flickpick.id} className="flick-card">
             
-              <img src={chicken} alt={flickpick.name} />
+            <img src={
+                flickpick.name === "Drama Movies" ? drama :
+                flickpick.name === "Horror Movies" ? horror :
+                flickpick.name === "Comedy Movies" ? comedy :
+                flickpick.name === "Action Movies" ? action :
+                flickpick.name === "Romance Movies" ? romance :
+                flickpick.name === "Thriller Movies" ? thriller :
+                flickpick.name === "Sci-Fi Movies" ? scifi :
+                flickpick.name === "Fantasy Movies" ? fantasy :""} alt={flickpick.name} />
               <h4 className="card-title work-sans">{flickpick.name}</h4>
           </div>
           
