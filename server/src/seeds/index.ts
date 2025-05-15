2636c291abf03f856c47053cb715bee9f8dfa720
zachmc27 <dzdudes1213@gmail.com>
2025-05-15 12:13:39 -0500

    removed password hashing from authRoutes.ts as a temporary fix. also removed user seeds so that creating a profile works.

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -3,6 +3,23 @@ import { seedGenres } from './genre-seeds.js';
 import sequelize from '../config/connection.js';
 import { createFlickPickList} from '../routes/api/flickPickListAPI.js';
 import { seedMedia } from './media-seeds.js';
+import { User } from '../models/index.js';
+import { Genre } from '../models/genres.js';
+import { Media } from '../models/media.js';
+
+
+const cleanDatabase = async (): Promise<void> => {
+  try {
+    console.log('\n----- CLEANING DATABASE -----\n');
+    await User.truncate({ cascade: true });
+    await Genre.truncate({ cascade: true });
+    await Media.truncate({ cascade: true });
+    console.log('\n----- DATABASE CLEANED -----\n');
+  } catch (error) {
+    console.error('Error cleaning database:', error);
+    throw error;
+  }
+};
 
 
 const seedAll = async (): Promise<void> => {
@@ -10,6 +27,8 @@ const seedAll = async (): Promise<void> => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
     
+    await cleanDatabase();
+    console.log('\n----- DATABASE CLEANED -----\n');
     await createFlickPickList();
     await seedGenres();
     await seedUsers();
